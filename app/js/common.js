d728a5e6e4cfa010d645b0c65fec1e205174103c
Jazzyst <andriy.zhuranlov@gmail.com>
2017-08-09 18:05:18 +0300

    added header

---
+++ app/js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+	$("nav li").click(function(){
+		$("nav li").removeClass("active");
+		$(this).addClass("active");
+	});
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
